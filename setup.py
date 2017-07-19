68ea49f46a3e0cb8ca2895d84cf6288cc47bc36e
richie serna <richie@balancedpayments.com>
2017-07-19 04:09:48 -0700

    add correct tar bell link

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
              .match(open('wac.py').read())
              .group(1)),
     url='https://github.com/finix-payments/fnx-wac',
-    download_url="https://github.com/finix-payments/fnx-wac-test/archive/0.3.tar.gz",
+    download_url="https://github.com/finix-payments/fnx-wac/archive/0.4.tar.gz",
     license=open('LICENSE').read(),
     author='Finix',
     author_email='dev@finixpayments.com',
